bdf6dd09b757d9b347d2fdf0a6c26ae7ce2ae6b2
zabih arab <zabih.arab1376@gmail.com>
2021-02-10 12:48:07 +0330

    debug heroku errors2

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ app.get(/.*/, function(req,res) {
   res.sendFile(__dirname + "/dist/index.html")
 });
 
-app.listen(port)
+// app.listen(port)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log('Listening on port: ' + port)
