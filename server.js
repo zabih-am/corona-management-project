44920dd2ca527bf4c42d68c2e89297d560692d1b
zabih arab <zabih.arab1376@gmail.com>
2021-02-10 14:56:58 +0330

    changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 const app = express()
 // const serveStatic = require('serve-static')
 // const path = require('path')
